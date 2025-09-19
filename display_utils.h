e0c7a276a07998cc8988bdd065750c934e4a914e
besthathacker <aiden123o@outlook.com>
2025-09-18 19:44:58 -0700

    Update display_utils.h

--- display_utils.h
+++ display_utils.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <stdint.h>
 
-#define DISPLAY_WIDTH 800
-#define DISPLAY_HEIGHT 480
+#define DISPLAY_WIDTH 320
+#define DISPLAY_HEIGHT 240
 
-void compose_display(uint8_t *buffer, const char *time_str);
-void draw_time(uint8_t *buffer, const char *time_str);
-void draw_glowing_box(uint8_t *buffer, int x, int y, int w, int h);
-void draw_rectangle(uint8_t *buffer, int x, int y, int w, int h, uint32_t color);
-void draw_circle(uint8_t *buffer, int x0, int y0, int radius, uint32_t color);
+// RGB565: 16-bit color
+extern uint16_t display_buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
-extern uint8_t display_buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT * 3];
+void compose_display(uint16_t *buffer, const char *time_str);
+void draw_time(uint16_t *buffer, const char *time_str);
+void draw_glowing_box(uint16_t *buffer, int x, int y, int w, int h);
+void draw_rectangle(uint16_t *buffer, int x, int y, int w, int h, uint16_t color);
+void draw_circle(uint16_t *buffer, int x0, int y0, int radius, uint16_t color);
