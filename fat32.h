5fe16ad47b28111bc59a1859c17132aee2a36678
besthathacker <aiden123o@outlook.com>
2025-09-20 11:25:31 -0700

    Create fat32.h

---
+++ fat32.h
@@ -0,0 +1,10 @@
+#ifndef FAT32_H
+#define FAT32_H
+#include <stdint.h>
+
+#define FAT32_IMAGE_SIZE (1024*1024)
+
+void fat32_init(uint8_t *disk_image);
+void fat32_place_file(uint8_t *disk_image,const char *filename,uint8_t *data,size_t len);
+
+#endif
